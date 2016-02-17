05834f896ad3484ff58cf7c0a0657ee836d20902
jalexanderqed <jalexander.qed@gmail.com>
2016-02-16 17:16:12 -0800

    Finished basic algorithm with hard-coded test case. Not wall-learning enabled yet.

--- Main.cpp
+++ Main.cpp
@@ -6,8 +6,11 @@ using namespace std;
 #define WIDTH 16
 #define HEIGHT 16
 
-#define CENTER_X 7
-#define CENTER_Y 7
+#define CENTER_X 13
+#define CENTER_Y 4
+
+#define START_X 0
+#define START_Y 0
 
 #define NORTH 1
 #define SOUTH 2
@@ -85,31 +88,42 @@ public:
         return (c_visitedFrom >> 4) & 15;
     };
 
-    void setVisitedFromX(uint8_t x) {
+    void setVisitedFromX(const uint8_t& x) {
         c_visitedFrom = (c_visitedFrom & 240) | x;
     };
 
-    void setVisitedFromY(uint8_t y) {
+    void setVisitedFromY(const uint8_t& y) {
         c_visitedFrom = (c_visitedFrom & 15) | (y << 4);
     };
 };
 
-struct QueueNode {
-    uint8_t x;
-    uint8_t y;
+class QueueNode {
+private:
+    uint8_t pos;
+
+public:
     QueueNode *prev;
 
-    QueueNode(uint8_t nx, uint8_t ny){
-        x = nx;
-        y = ny;
+    QueueNode(uint8_t x, uint8_t y){
+        pos = (pos & 240) | x;
+        pos = (pos & 15) | (y << 4);
+        prev = NULL;
+    }
+
+    uint8_t x(){
+        return pos & 15;
+    }
+
+    uint8_t y(){
+        return (pos >> 4) & 15;
     }
 };
 
 Space g_board[WIDTH][HEIGHT];
 QueueNode *g_queueHead;
 QueueNode *g_queueTail;
-uint8_t currentX;
-uint8_t currentY;
+uint8_t g_currentX;
+uint8_t g_currentY;
 
 void resetFullPath() {
     for(int i = 0; i < WIDTH; i++){
@@ -129,6 +143,14 @@ void resetPartPath() {
     }
 }
 
+void unvisitAll(){
+    for(int i = 0; i < WIDTH; i++){
+        for(int j = 0; j < HEIGHT; j++){
+            g_board[i][j].unVisit();
+        }
+    }
+}
+
 void initializeWalls(){
     for(int i = 0; i < WIDTH; i++){
         g_board[i][0].hasNorthWall();
@@ -141,11 +163,14 @@ void initializeWalls(){
 }
 
 void print(){
-    char blue[] = { 0x1b, '[', '3', '4', 'm', 0 };
-    char red[] = { 0x1b, '[', '3', '1', 'm', 0 };
-    char yellow[] = { 0x1b, '[', '3', '3', 'm', 0 };
+    char blue[] = { 0x1b, '[', '4', '4', 'm', 0 };
+    char red[] = { 0x1b, '[', '4', '1', 'm', 0 };
+    char yellow[] = { 0x1b, '[', '4', '3', 'm', 0 };
+    char cyan[] = { 0x1b, '[', '4', '6', 'm', 0 };
+    char magenta[] = { 0x1b, '[', '4', '5', 'm', 0 };
+    char green[] = { 0x1b, '[', '4', '2', 'm', 0 };
     char underline[] = {0x1b, '[', '4', 'm', 0};
-    char noColor[] = { 0x1b, '[', '3', '9', 'm', 0};
+    char noColor[] = { 0x1b, '[', '4', '9', 'm', 0};
     char noFormat[] = {0x1b, '[', '0', 'm', 0};
 
     cout << noColor;
@@ -162,15 +187,24 @@ void print(){
         cout << '|';
         cout << noFormat;
         for(int x = 0; x < WIDTH; x++){
-            if(x == CENTER_X && y == CENTER_Y) cout << red;
-            else {
-                if(g_board[x][y].c_fullPathPos != 0) cout << yellow;
+            if(x == g_currentX && y == g_currentY) cout << green;
+            else{
+                if (x == START_X && y == START_Y) cout << yellow;
                 else {
-                    if (g_board[x][y].visited()) cout << blue;
+                    if (x == CENTER_X && y == CENTER_Y) cout << red;
+                    else {
+                        if (g_board[x][y].c_fullPathPos != 0) cout << yellow;
+                        else {
+                            if (g_board[x][y].c_partPathPos != 0) cout << cyan;
+                            else {
+                                if (g_board[x][y].visited()) cout << blue;
+                            }
+                        }
+                    }
                 }
             }
             if(g_board[x][y].southWall()) cout << underline;
-            cout << (x == currentX && y == currentY ? 'X' : 'O');
+            cout << (x == g_currentX && y == g_currentY ? 'X' : 'O');
             cout << noColor;
             if(g_board[x][y].eastWall()) cout << '|';
             else cout << ' ';
@@ -179,12 +213,13 @@ void print(){
         cout << endl;
     }
     cout << endl;
+    usleep(50000);
 }
 
 QueueNode* dequeue(){
     QueueNode* res = g_queueHead;
     if(res != NULL) g_queueHead = g_queueHead->prev;
-    if(g_queueHead == NULL) g_queueTail == NULL;
+    if(g_queueHead == NULL) g_queueTail = NULL;
     return res;
 }
 
@@ -202,7 +237,84 @@ void clearQueue(){
     while(g_queueTail != NULL) delete dequeue();
 }
 
-void floodFillFrom(uint8_t startX, uint8_t startY){
+uint8_t moveTo(const uint8_t& x, const uint8_t& y){
+    g_currentX = x;
+    g_currentY = y;
+    print();
+    return 0;
+}
+
+QueueNode nextInMainPath(const uint8_t& x, const uint8_t& y){
+    uint8_t nextInd = g_board[x][y].c_fullPathPos - 1;
+    if(nextInd == 0) return QueueNode(CENTER_X, CENTER_Y);
+    if(g_board[x - 1][y].c_fullPathPos == nextInd) return QueueNode(x - 1, y);
+    if(g_board[x + 1][y].c_fullPathPos == nextInd) return QueueNode(x + 1, y);
+    if(g_board[x][y - 1].c_fullPathPos == nextInd) return QueueNode(x, y - 1);
+    if(g_board[x][y + 1].c_fullPathPos == nextInd) return QueueNode(x, y + 1);
+}
+
+QueueNode prevInMainPath(const uint8_t& x, const uint8_t& y){
+    uint8_t nextInd = g_board[x][y].c_fullPathPos + 1;
+    if(nextInd == 0) return QueueNode(CENTER_X, CENTER_Y);
+    if(g_board[x - 1][y].c_fullPathPos == nextInd) return QueueNode(x - 1, y);
+    if(g_board[x + 1][y].c_fullPathPos == nextInd) return QueueNode(x + 1, y);
+    if(g_board[x][y - 1].c_fullPathPos == nextInd) return QueueNode(x, y - 1);
+    if(g_board[x][y + 1].c_fullPathPos == nextInd) return QueueNode(x, y + 1);
+}
+
+bool pointInMainPath(const uint8_t& x, const uint8_t& y){
+    uint8_t currentX = START_X;
+    uint8_t currentY = START_Y;
+    if(currentX == x && currentY == y) return true;
+
+    while(currentX != CENTER_X || currentY != CENTER_Y){
+        QueueNode nextPoint = nextInMainPath(currentX, currentY);
+        currentX = nextPoint.x();
+        currentY = nextPoint.y();
+        if(currentX == x && currentY == y) return true;
+    }
+    return false;
+}
+
+QueueNode nextInPartPath(const uint8_t& x, const uint8_t& y){
+    uint8_t nextInd = g_board[x][y].c_partPathPos - 1;
+    if(nextInd == 0){
+        if(pointInMainPath(x - 1, y) && !g_board[x][y].westWall()) return QueueNode(x - 1, y);
+        if(pointInMainPath(x + 1, y) && !g_board[x][y].eastWall()) return QueueNode(x + 1, y);
+        if(pointInMainPath(x, y - 1) && !g_board[x][y].northWall()) return QueueNode(x, y - 1);
+        if(pointInMainPath(x, y + 1) && !g_board[x][y].southWall()) return QueueNode(x, y + 1);
+    }
+    if(g_board[x - 1][y].c_partPathPos == nextInd) return QueueNode(x - 1, y);
+    if(g_board[x + 1][y].c_partPathPos == nextInd) return QueueNode(x + 1, y);
+    if(g_board[x][y - 1].c_partPathPos == nextInd) return QueueNode(x, y - 1);
+    if(g_board[x][y + 1].c_partPathPos == nextInd) return QueueNode(x, y + 1);
+}
+
+uint8_t followMain(){
+    while(g_currentX != CENTER_X || g_currentY != CENTER_Y) {
+        QueueNode next = nextInMainPath(g_currentX, g_currentY);
+        if (moveTo(next.x(), next.y()) != 0) return 1;
+    }
+    return 0;
+}
+
+uint8_t followMainBack(){
+    while(g_currentX != START_X || g_currentY != START_Y) {
+        QueueNode next = prevInMainPath(g_currentX, g_currentY);
+        if (moveTo(next.x(), next.y()) != 0) return 1;
+    }
+    return 0;
+}
+
+uint8_t followPart(){
+    while(!pointInMainPath(g_currentX, g_currentY)) {
+        QueueNode next = nextInPartPath(g_currentX, g_currentY);
+        if (moveTo(next.x(), next.y()) != 0) return 1;
+    }
+    return 0;
+}
+
+void floodFillFrom(const uint8_t& startX, const uint8_t& startY){
     if(!g_board[startX][startY].northWall() && !g_board[startX][startY - 1].visited()){
         g_board[startX][startY - 1].visit();
         g_board[startX][startY - 1].setVisitedFromX(startX);
@@ -232,6 +344,76 @@ void floodFillFrom(uint8_t startX, uint8_t startY){
     }
 }
 
+uint8_t floodFillMain(){
+    uint8_t currentX = START_X;
+    uint8_t currentY = START_Y;
+
+    resetFullPath();
+    clearQueue();
+
+    g_board[START_X][START_Y].visit();
+
+    while(!g_board[CENTER_X][CENTER_Y].visited()){
+        floodFillFrom(currentX, currentY);
+
+        if(g_queueHead == NULL) return 1;
+
+        QueueNode* next = dequeue();
+        currentX = next->x();
+        currentY = next->y();
+        delete next;
+        print();
+    }
+
+    uint8_t counter = 0;
+    currentX = CENTER_X;
+    currentY = CENTER_Y;
+    while(currentX != START_X || currentY != START_Y){
+        g_board[currentX][currentY].c_fullPathPos = counter;
+        counter++;
+        uint8_t tempX = currentX;
+        currentX = g_board[currentX][currentY].visitedFromX();
+        currentY = g_board[tempX][currentY].visitedFromY();
+    }
+    g_board[currentX][currentY].c_fullPathPos = counter;
+
+    return 0;
+}
+
+uint8_t floodFillPartial(){
+    uint8_t currentX = g_currentX;
+    uint8_t currentY = g_currentY;
+
+    resetPartPath();
+    clearQueue();
+
+    g_board[currentX][currentY].visit();
+
+    while(!pointInMainPath(currentX, currentY)){
+        floodFillFrom(currentX, currentY);
+
+        if(g_queueHead == NULL) return 1;
+
+        QueueNode* next = dequeue();
+        currentX = next->x();
+        currentY = next->y();
+        delete next;
+        print();
+    }
+
+    uint8_t counter = 0;
+    while(currentX != g_currentX || currentY != g_currentY){
+        g_board[currentX][currentY].c_partPathPos = counter;
+        counter++;
+        uint8_t tempX = currentX;
+        currentX = g_board[currentX][currentY].visitedFromX();
+        currentY = g_board[tempX][currentY].visitedFromY();
+    }
+    g_board[currentX][currentY].c_partPathPos = counter;
+
+    return 0;
+}
+
 void printDebug(uint8_t size){
     for(int i = 0; i < size; i++){
         for(int j = 0; j < size; j++){
@@ -248,48 +430,43 @@ void printDebug(uint8_t size){
     }
 }
 
-uint8_t floodFillMain(uint8_t startX, uint8_t startY, uint8_t goalX, uint8_t goalY){
-    uint8_t originalStartX = startX;
-    uint8_t  originalStartY = startY;
-
-    resetFullPath();
-    clearQueue();
-
-    g_board[startX][startY].visit();
-
-    while(!g_board[goalX][goalY].visited()){
-        floodFillFrom(startX, startY);
-
-        if(g_queueHead == NULL) return 1;
-
-        QueueNode* next = dequeue();
-        startX = next->x;
-        startY = next->y;
-        delete next;
-        print();
-        usleep(250000);
-    }
-
-    uint8_t counter = 0;
-    startX = goalX;
-    startY = goalY;
-    while(startX != originalStartX || startY != originalStartY){
-        g_board[startX][startY].c_fullPathPos = counter;
-        counter++;
-        uint8_t tempX = startX;
-        startX = g_board[startX][startY].visitedFromX();
-        startY = g_board[tempX][startY].visitedFromY();
-    }
-
-    return 0;
-}
-
 int main() {
-    currentX = 0;
-    currentY = 0;
-    resetFullPath();
-    resetPartPath();
+    g_currentX = 5;
+    g_currentY = 10;
     initializeWalls();
-    (int)floodFillMain(currentX, currentY, CENTER_X, CENTER_Y);
+
+    g_board[3][4].hasEastWall();
+    g_board[4][4].hasWestWall();
+    g_board[5][5].hasSouthWall();
+    g_board[5][6].hasNorthWall();
+
+    while(true) {
+        resetFullPath();
+        resetPartPath();
+        if(floodFillMain() != 0){
+            cout << "Could not flood fill main" << endl;
+            return 1;
+        }
+        print();
+        if(floodFillPartial() != 0){
+            cout << "Could not flood fill part" << endl;
+            return 1;
+        }
+        print();
+        unvisitAll();
+        if(followPart() != 0){
+            cout << "Could not follow part" << endl;
+            return 1;
+        }
+        if(followMain() != 0) {
+            cout << "Could not follow main" << endl;
+            return 1;
+        }
+        if(followMainBack() == 0) break;
+        else {
+            cout << "Could not follow main back" << endl;
+            return 1;
+        }
+    }
     print();
 }
